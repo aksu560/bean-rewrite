eed4b9b0cbde383cb5c66f3eb13d844a4a541564
Aksu <hypernaurisukko@gmail.com>
2020-01-08 20:02:00 +0200

    Secured mysql

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -6,3 +6,6 @@ apt-get update
 apt-get install -y mysql-server
 sudo systemctl start mysql
 sudo /etc/init.d/mysql restart
+
+mysql-uroot -uroot -proot -e "CREATE DATABASE IF NOT EXISTS bot"
+mysql-uroot -uroot -proot -e "grant all privileges on bot.* to 'bean' identified by 'root'"
