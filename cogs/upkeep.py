f73e27cb81c9f779370bffbfd6039579bdccb588
Aksu <hypernaurisukko@gmail.com>
2020-01-08 17:33:28 +0200

    Added upkeep cog

---
+++ cogs/upkeep.py
@@ -0,0 +1,56 @@
+from discord.ext import commands
+
+
+class Upkeep(commands.Cog):
+    def __init__(self, client: commands.Bot):
+        self.client = client
+
+    @commands.command()
+    async def reload(self, ctx):
+        """Reload all the cogs"""
+        reloadMessage = "Reloading cogs:```css\n"
+        failedOne = False
+
+        for cog in self.client.allCogs:
+            try:
+                self.client.unload_extension(cog)
+                self.client.load_extension(cog)
+                reloadMessage += f"{cog[4:]} - success\n"
+            except Exception as err:
+                failedOne = True
+                reloadMessage += f"{cog[4:]} - failed\n"
+                print(f"[reload] Failed to reload cog \"{cog}\" [{type(err).__name__}: {err}]")
+
+        reloadMessage += "```"
+        reloadMessage += "**Something's wrong!**" if failedOne == True else ""
+        await ctx.send(reloadMessage)
+
+    @commands.command()
+    async def die(self, ctx):
+        """Kill the bot"""
+        await ctx.send("Time for me to die :<")
+        exit()
+
+    @commands.command(brief="[channel/user ID] [message]")
+    async def messageto(self, ctx, target: str, *, msg: str):
+        """Send a custom message to any channel or user"""
+        channel = self.client.get_channel(int(target))
+        if channel is None:
+            channel = await self.client.fetch_user(int(target))
+        await channel.send(msg)
+
+    @messageto.error
+    async def messageTo_eh(self, ctx: commands.Context, err: Exception):
+        if isinstance(err, commands.MissingRequiredArgument):
+            await ctx.send(f"{ctx.author.mention} you forgot something... Baka...")
+        elif isinstance(err, commands.CommandInvokeError):
+            await ctx.send(f"{ctx.author.mention} invalid channel/user ID... Baka...")
+
+    @commands.command()
+    async def sep(self, ctx):
+        """Just sends some separating lines to the server console. Used for debugging"""
+        print("-------")
+
+
+def setup(client: commands.Bot):
+    client.add_cog(Upkeep(client))
--- main.py
+++ main.py
@@ -9,9 +9,29 @@ auth = open(os.getcwd() + "/auth.ini")
 client.cfgParser.read_file(auth)
 clientKey = client.cfgParser.get("discord", "key")
 
+client.allCogs = [
+    "cogs.upkeep",
+]
+
+
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("==== Starting to initialize ====")
+
+    print("Loading cogs...")
+    for cog in client.allCogs:
+        try:
+            client.load_extension(cog)
+            print(f"Successfully loaded cog \"{cog}\"")
+        except Exception as err:
+            print(f"Failed to load cog \"{cog}\" [{type(err).__name__}: {err}]")
+
+    print(f"-- Connected to {len(client.servers)} servers:")
+    for server in client.servers:
+        print(f":: {server.name}")
+
+    print("==== Initialization success! ====")
+
 
 @client.event
 async def on_message(message):
@@ -20,5 +40,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
 
 client.run(clientKey)
