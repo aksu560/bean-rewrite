6f3883e13a6f2632a111582b6758bd6924333cce
Aksu <hypernaurisukko@gmail.com>
2020-01-08 19:57:29 +0200

    Secured mysql

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -4,7 +4,5 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 apt-get update
 apt-get install -y mysql-server
-echo "root" | sudo mysql_secure_installation utility
 sudo systemctl start mysql
-echo "root" | mysqladmin -u root -p version
 sudo /etc/init.d/mysql restart
