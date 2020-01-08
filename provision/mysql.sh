99ee4c2140b56519c6382310f5e1308301a645fb
Aksu <hypernaurisukko@gmail.com>
2020-01-08 19:52:48 +0200

    Secured mysql

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y vim curl python-software-properties
 sudo apt-get update
 sudo apt-get -y install mysql-server
-sudo mysql_secure_installation utility
+echo "root" | sudo mysql_secure_installation utility
 sudo systemctl start mysql
 echo "root" | mysqladmin -u root -p version
 sudo /etc/init.d/mysql restart
