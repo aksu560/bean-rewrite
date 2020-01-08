715650f29576a7d227a4cf7305c8a67c5700e168
Aksu <hypernaurisukko@gmail.com>
2020-01-08 22:10:23 +0200

    initial sql testing

--- tools/beanbase.py
+++ tools/beanbase.py
@@ -5,7 +5,7 @@ import sqlalchemy_utils as db_utils
 
 # Create the database if it does not exist
 if not db_utils.database_exists('mysql://root:root@localhost/beanbase'):
-    db_utils.database_create('mysql://root:root@localhost/beanbase')
+    db_utils.create_database('mysql://root:root@localhost/beanbase')
 
 # Declaring the base class for declarative structures
 Base = declarative_base()
@@ -14,6 +14,10 @@ Base = declarative_base()
 class Servers(Base):
     __tablename__ = 'servers'
 
-    server_id = Column(String, primary_key=True)
+    server_id = Column(String(17), primary_key=True)
     server_premium = Column('Premium Server', Boolean)
     server_announce = Column('Announcements enabled', Boolean)
+
+    def __repr__(self):
+        return [self.server_id, self.server_premium, self.server_announce]
+
