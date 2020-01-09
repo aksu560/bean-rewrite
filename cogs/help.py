bf39790ffc618d758715ade6a8976a58a568fc77
Aksu <hypernaurisukko@gmail.com>
2020-01-09 17:16:03 +0200

    minor edits

--- cogs/help.py
+++ cogs/help.py
@@ -7,15 +7,25 @@ class Help(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def Help(self, ctx: commands.Context):
+    async def Help(self, ctx, targetcog: str = ""):
         """Is a very helpful command"""
-        commandsText = f"Here are all the cogs available, please use &help [cogname] for" \
-                       f" help with individual commands```css\n"
 
-        for cog in self.client.allCogs:
-            commandsText += f".{cog}\n"
+        if targetcog == "":
+            commandsText = f"Here are all the cogs available, please use &help [cogname] for" \
+                           f" help with individual commands```css\n"
 
-        # Cuts the output to multiple commands if the output would go over Discord's character limit
+            for cog in self.client.allCogs:
+                commandsText += f".{cog}\n"
+        else:
+            targetcog = self.client.get_cog(targetcog)
+            commandsText = f"Here are all the commands in {targetcog.qualified_name}```css"
+
+            for command in targetcog.get_commands():
+                commandsText += f"\n    &{command.name} "
+                commandsText += f"{command.brief} " if command.brief is not None else ""
+                commandsText += f"/* {command.help} */"
+
+        # Cuts the output to multiple messages if the output would go over Discord's character limit
         if len(commandsText) > 2000:
             texts = []
             pos1 = commandsText.find('\n', 1700, 1900)
