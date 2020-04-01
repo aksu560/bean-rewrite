078aa6e7049a6af4fb6cd2c2834ae412537593b2
Aksu <hypernaurisukko@gmail.com>
2020-04-01 10:49:16 +0300

    wip

--- main.py
+++ main.py
@@ -53,18 +53,6 @@ async def on_ready():
 
 @client.event
 async def on_message(msg):
-    print(msg.content)
-    if msg.content.startswith("&"):
-        if str(datetime.date.today().month) == "4":
-            print("Date works")
-            if str(datetime.date.today().day) == "1" or str(datetime.date.day) == "2":
-                print("and so does the day :D")
-                if random.randint(0, 1) == 1:
-                    print("bamboozled")
-                    await msg.channel.send("No")
-                    return
-                else:
-                    print("Not Bamboozled")
     await client.process_commands(msg)
 
 
