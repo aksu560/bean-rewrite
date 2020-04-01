76c705eb2a5d4f777d750c2b8ef5197c010ed902
Aksu <hypernaurisukko@gmail.com>
2020-04-01 10:53:56 +0300

    wip

--- cogs/shadownet.py
+++ cogs/shadownet.py
@@ -8,6 +8,7 @@ from cogs.tools import shadownet_wiki
 from cogs.tools.reddit import reddit
 from fuzzywuzzy import fuzz
 import math
+import datetime
 
 
 class Shadownet(commands.Cog):
@@ -17,7 +18,15 @@ class Shadownet(commands.Cog):
         self.client = client
         self.approved_guilds = [160034813040918528, 438287744683474947]
 
-    def cog_check(self, ctx):
+    async def cog_check(self, ctx):
+
+        if str(datetime.date.today().month) == "4":
+            print("Date works")
+            if str(datetime.date.today().day) == "1" or str(datetime.date.day) == "2":
+                print("and so does the day :D")
+                if random.randint(0, 1) == 1:
+                    await ctx.send (random.choice(["No", "Heck off!", "Do it yourself", "Seems like effort", "Nah", "Not feeling like it", "How would you feel if I asked you constantly for stuff?"]))
+                    return
 
         return ctx.guild.id in self.approved_guilds
 
