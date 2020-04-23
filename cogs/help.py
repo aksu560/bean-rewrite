31e68a24471fab675fa1ee505d40f2ff86b2594d
Aksu <hypernaurisukko@gmail.com>
2020-04-23 12:32:42 +0300

    Dumb typo

--- cogs/help.py
+++ cogs/help.py
@@ -84,7 +84,7 @@ class Help(commands.Cog):
         else:
             await ctx.send(commands_text + "```")
 
-    @commands.command
+    @commands.command()
     async def ServerInfo(self, ctx):
         server_admins = ""
         for admin in beanbase.GetServerAdmins(str(ctx.guild.id)):
